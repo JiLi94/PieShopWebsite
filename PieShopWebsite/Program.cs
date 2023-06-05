29ea20a5bf46523827dce41c2dccd08bb406b042
Ji Li <Ji.Li@globirdenergy.com.au>
2023-06-05 20:28:35 +1000

    project.init

--- PieShopWebsite/Program.cs
+++ PieShopWebsite/Program.cs
@@ -1,6 +1,19 @@
+// load the appsetting.json file and kestrel server
 var builder = WebApplication.CreateBuilder(args);
+
+// make sure the app knows MVC, enable MVC
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Middlewares
+app.UseStaticFiles();
+
+if (app.Environment.IsDevelopment()) {
+    app.UseDeveloperExceptionPage();
+}
+
+// endpoint middleware
+app.MapDefaultControllerRoute();
 
 app.Run();
