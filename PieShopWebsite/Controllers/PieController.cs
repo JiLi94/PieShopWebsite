ab610fa8f3e615dd0ed789a677ade1fd336ffdc4
Ji Li <Ji.Li@globirdenergy.com.au>
2023-06-09 13:57:11 +1000

    added the first page: ./pie/list

---
+++ PieShopWebsite/Controllers/PieController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PieShopWebsite.Models;
+using PieShopWebsite.ViewModels;
+
+namespace PieShopWebsite.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        //constructor injection
+        //an instance of the repository implementing the IPieRepository interface and an instance of the repository implementing
+        //the ICategoryRepository interface will be provided when creating an instance of the PieController
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public IActionResult List()
+        {
+            //ViewBag.CurrentCategory = "Cheese Cakes";
+            // pass the collection of pies from model to the view
+            //return View(_pieRepository.AllPies);
+            PieListViewModel pieListViewModel = new PieListViewModel(_pieRepository.AllPies, "Cheese Cakes");
+            return View(pieListViewModel);
+        }
+    }
+}
---
+++ PieShopWebsite/ViewModels/PieListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using PieShopWebsite.Models;
+
+namespace PieShopWebsite.ViewModels
+{
+    public class PieListViewModel
+    {
+        public IEnumerable<Pie> Pies { get;}
+        public string ? CurrentCategoty { get;}
+
+        public PieListViewModel(IEnumerable<Pie> pies, string? currentCategoty)
+        {
+            Pies = pies;
+            CurrentCategoty = currentCategoty;
+        }
+    }
+}
