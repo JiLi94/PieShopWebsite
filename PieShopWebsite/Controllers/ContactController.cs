e671dafb8e25ace51ba800026edc4b29352f37e0
Ji Li <liji042294@gmail.com>
2023-06-11 22:54:32 +1000

    added a email tag helper

---
+++ PieShopWebsite/Controllers/ContactController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PieShopWebsite.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
