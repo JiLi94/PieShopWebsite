5864881ff534de3764fd132880d63b714d62a11b
Ji Li <liji042294@gmail.com>
2023-06-11 22:27:37 +1000

    show the item count in the shopping cart in the top right corner of the page

---
+++ PieShopWebsite/Components/ShoppingCartSummary.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using BethanysPieShop.Models;
+using Microsoft.AspNetCore.Mvc;
+using PieShopWebsite.Models;
+using PieShopWebsite.ViewModels;
+
+namespace PieShopWebsite.Components
+{
+	public class ShoppingCartSummary: ViewComponent
+	{
+		private readonly IShoppingCart _shoppingCart;
+
+		public ShoppingCartSummary(IShoppingCart shoppingCart)
+		{
+			_shoppingCart = shoppingCart;
+		}
+
+        public IViewComponentResult Invoke()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            var shoppingCartViewModel = new ShoppingCartViewModel(_shoppingCart, _shoppingCart.GetShoppingCartTotal());
+
+            return View(shoppingCartViewModel);
+        }
+    }
+}
+
